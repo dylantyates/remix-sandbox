0bfb944d359235f9f3f2712cdc3082e80a42d96f
Dylan Yates <dyates.texas@gmail.com>
2022-11-13 14:34:12 -0600

    style: change seed stuff

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,14 +4,14 @@ import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const email = "dylan@remix.run";
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const hashedPassword = await bcrypt.hash("dylaniscool", 10);
 
   const user = await prisma.user.create({
     data: {
@@ -26,16 +26,8 @@ async function seed() {
 
   await prisma.note.create({
     data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
+      title: "Hello, world!",
+      body: "So begins the remix journey...",
       userId: user.id,
     },
   });
